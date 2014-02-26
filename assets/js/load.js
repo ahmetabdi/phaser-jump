19247a6cfcd01bee6ac075276312feab9d23a689
Ahmet <ahmetabdi@gmail.com>
2014-02-26 22:10:16 +0000

    Player animations

--- assets/js/load.js
+++ assets/js/load.js
@@ -16,20 +16,23 @@ Game.Load.prototype = {
     game.stage.backgroundColor = '#9b59b6';
     label1 = game.add.text(Math.floor(w/2), Math.floor(h/2)-20, 'loading...', { font: '30px Arial', fill: '#fff' });
     label2 = game.add.text(Math.floor(w/2)+0.5, Math.floor(h/2)+20+0.5, 'loading...', { font: '16px Arial', fill: '#fff' });
-		label1.anchor.setTo(0.5, 0.5);
-		label2.anchor.setTo(0.5, 0.5);
+	label1.anchor.setTo(0.5, 0.5);
+	label2.anchor.setTo(0.5, 0.5);
 
-		game.load.spritesheet('tiles', 'assets/sprites/platformer_tiles.png', 16, 16);
-		game.load.spritesheet('mummy', 'assets/sprites/321.png', 37, 45, 1);
+	game.load.spritesheet('tiles', 'assets/sprites/platformer_tiles.png', 16, 16);
+	game.load.spritesheet('player_idle_first', 'assets/sprites/player_idle.png', 64, 64, 1);
 
-		game.load.image('test', 'assets/images/blood_a/blood_a_0001.png');
+	game.load.image('test', 'assets/images/blood_a/blood_a_0001.png');
 
-		game.load.spritesheet('blood', 'assets/sprites/blood.png', 32, 32, 6);
+	game.load.spritesheet('blood', 'assets/sprites/blood.png', 32, 32, 6);
 
     game.load.spritesheet('lightning_particles', 'assets/sprites/blood.png', 32, 32, 6);
 
     game.load.image('lightning_bolt', 'assets/images/bolt_tesla/bolt_tesla_0010.png');
 
+    game.load.spritesheet('player_jump', 'assets/sprites/player_jump.png', 64, 64, 6);
+    game.load.spritesheet('player_idle', 'assets/sprites/player_idle.png', 64, 64, 6);
+    game.load.spritesheet('player_running', 'assets/sprites/player_running.png', 64, 64, 6);
 
 	},
 	create: function () {
--- assets/js/play.js
+++ assets/js/play.js
@@ -29,59 +29,79 @@ Game.Play.prototype = {
 
 	create: function () {
 
-		game.stage.backgroundColor = '#2d2d2d';
+		game.stage.backgroundColor = '#A67D3D';
 
-			cubes = game.add.group();
-      cubes.createMultiple(20, 'cube');
+		cubes = game.add.group();
+    cubes.createMultiple(20, 'cube');
 
-	    tiles = game.add.group();
+    tiles = game.add.group();
 
-	    for (var x = 0; x < 40; x++)
-	    {
-	        var tile = tiles.create(0 + (x * 15), h*2/3+22, 'tiles', 4);
-	        tile.body.immovable = true;
-	    }
+    for (var x = 0; x < 40; x++)
+    {
+        if (x == 0) { var tile = tiles.create(0 + (x * 15), h*2/3+22, 'tiles', 104); }
+        else if (x == 39) { var tile = tiles.create(0 + (x * 15), h*2/3+22, 'tiles', 106); }
+        else { var tile = tiles.create(0 + (x * 15), h*2/3+22, 'tiles', 105); }
+        
+        tile.body.immovable = true;
+    }
 
-	    this.start = 0;
-	    this.level = 0;
+    this.start = 0;
+    this.level = 0;
 
-	    this.player = this.game.add.sprite(80, h*1/3-50, 'mummy');
-			this.player.body.bounce.y = 0;
-			this.player.body.collideWorldBounds = true;
-	    this.player.body.velocity.x = 40;
-	    this.player.body.velocity.y = 120;
-	    this.player.body.acceleration.y = 500;
-    	this.player.anchor.setTo(0.5, 0.5);
+    this.player = this.game.add.sprite(80, h*1/3-50, 'player_idle_first');
+		this.player.body.bounce.y = 0;
+		this.player.body.collideWorldBounds = true;
+    this.player.body.velocity.x = 40;
+    this.player.body.velocity.y = 120;
+    this.player.body.acceleration.y = 500;
+  	this.player.anchor.setTo(0.5, 0.5);
 
-    	this.labelDeath = game.add.text(100, h-35, '0', { font: '18px Arial', fill: '#fff', align: 'center' });
-    	this.labelDeath.anchor.setTo(0.5, 0.5);
-      this.labelLevel = game.add.text(w-100+0.5, h-35, '1/'+map.length, { font: '18px Arial', fill: '#fff', align: 'center' });
-	    this.labelLevel.anchor.setTo(0.5, 0.5);
+  	this.labelDeath = game.add.text(100, h-35, '0', { font: '18px Arial', fill: '#fff', align: 'center' });
+  	this.labelDeath.anchor.setTo(0.5, 0.5);
+    this.labelLevel = game.add.text(w-100+0.5, h-35, '1/'+map.length, { font: '18px Arial', fill: '#fff', align: 'center' });
+    this.labelLevel.anchor.setTo(0.5, 0.5);
 
-	    this.emitter = game.add.emitter(0, 0, 10);
-	    this.emitter.makeParticles('blood');
-	    this.emitter.gravity = 0;
-	    this.emitter.minParticleSpeed.setTo(-200, -200);
-	    this.emitter.maxParticleSpeed.setTo(200, 200);
+    this.emitter = game.add.emitter(0, 0, 10);
+    this.emitter.makeParticles('blood');
+    this.emitter.gravity = 0;
+    this.emitter.minParticleSpeed.setTo(-200, -200);
+    this.emitter.maxParticleSpeed.setTo(200, 200);
 
-      this.emitter2 = game.add.emitter(0, 0, 50);
-      this.emitter2.makeParticles('lightning_particles');
-      this.emitter2.gravity = 1000;
-      this.emitter2.minParticleSpeed.setTo(-200, -200);
-      this.emitter2.maxParticleSpeed.setTo(700, 900);
+    this.emitter2 = game.add.emitter(0, 0, 50);
+    this.emitter2.makeParticles('lightning_particles');
+    this.emitter2.gravity = 1000;
+    this.emitter2.minParticleSpeed.setTo(-200, -200);
+    this.emitter2.maxParticleSpeed.setTo(700, 900);
 
-			this.cursor = this.game.input.keyboard.createCursorKeys();
-    	this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    this.player.loadTexture('player_idle', 0);
+    this.player.animations.add('player_idle');
+    this.player.loadTexture('player_jump', 0);
+    this.player.animations.add('player_jump');
+    this.player.loadTexture('player_running', 0);
+    this.player.animations.add('player_running');
+    this.player.loadTexture('player_idle', 0);
 
-    	this.loadLevel();
+		this.cursor = this.game.input.keyboard.createCursorKeys();
+  	this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+  	this.loadLevel();
 	},
+
+  render: function(){
+    game.debug.renderSpriteBody(this.player.body);
+  },
 
 	update: function() {
 		game.physics.collide(this.player, tiles);
 
+    if(this.player.body.touching.down && this.start == 0) {
+      this.player.animations.play('player_idle', 10, true);
+    }
+
 		//When spacebar is held down start the game
 		if (game.input.mousePointer.isDown && this.player.body.touching.down) {
         this.playerJump();
+        this.player.animations.play('player_jump', 10, false)
         if (this.start == 0) {
         	this.start = 1;
         }
@@ -91,13 +111,16 @@ Game.Play.prototype = {
     if (this.player.body.touching.down && this.start == 1) {
     	this.player.alive = true;
     	this.player.body.velocity.x = 180;
+      if(!game.input.mousePointer.isDown) {
+        this.player.animations.play('player_running', 10, true)
+      }
     }
 
     //Player global gravity when jumping etc
     this.player.body.gravity.y = 5;
 
     //If we reach the right side of the screen restart the view
-    if (this.player.x >= w - 20)
+    if (this.player.x >= w - 50)
 	    	this.loadLevel();
 
 	  //Check collision on players with obstacles(cubes)
@@ -118,7 +141,7 @@ Game.Play.prototype = {
 
 	initPlayer: function() {
 		this.player.body.gravity.y = 0;
-		this.player.x = 0;
+		this.player.x = 30;
     if (this.start == 1) {
       this.player.y = h*2/3+50-this.player.height/2-30;
     }
